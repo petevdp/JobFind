6b1396f2ca8c9dd22bbb6409c8b569043a3003a8
grey275 <pjvanderpol@gmail.com>
2019-12-14 15:14:10 -0800

    print valid search results given city and kewords

---
+++ design.md
@@ -0,0 +1,12 @@
+mvp: make one ip request based on user input, and return the results as a table?
+
+### DONE
+* get basic api request working
+
+### TODO
+* decide how to structure the resulting data for consumption
+
+### STRECH
+* unit testing
+* advanced search features
+* incremental searc
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const BASE_URL = "https://api.ziprecruiter.com/jobs/v1";
+console.log('env: ', process.env)
+const { REACT_APP_ZIP_RECRUITER_API_KEY } = process.env;
+
+async function zipFetch (city: string, search: string) {
+  const fullUrl = encodeURI(BASE_URL + `?api_key=${REACT_APP_ZIP_RECRUITER_API_KEY}&city=${city}&search=${search}`);
+  return (await fetch(fullUrl, {})).json();
+};
+
+interface AppState {
+  city: string;
+  search: string;
+}
+
 const App: React.FC = () => {
+  const [appState, setState] = useState({city: 'portland', search: 'python'} as AppState);
+  const {city, search} = appState;
+  useEffect(() => {
+    zipFetch(city, search).then((val) => console.log(val))
+      .then(out => console.log('out: ', out));
+  });
+
+  function onSubmit (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    // const out = zipFetch(city, search)
+    // console.log(out);
+  };
+
+  function onSearchChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setState({...appState, search: e.target.value});
+  }
+
+  function onCityChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setState({...appState, city: e.target.value});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input name="city" type="text" value={city} onChange={onCityChange} placeholder="City"/>
+        <input name="search" type="text" value={search} onChange={onSearchChange} placeholder="job keywords"/>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
