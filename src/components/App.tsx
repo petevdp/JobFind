15037973bbad7b61d991195ab72f7aee6191c04d
grey275 <pjvanderpol@gmail.com>
2019-12-15 12:40:38 -0800

    add variable status message

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,52 +1,61 @@
-import React, { useState, useReducer, useEffect } from 'react';
-import '../styles/App.scss';
-import JobList from './JobList';
+import React, { useState, useReducer, useEffect } from "react";
+import "../styles/App.scss";
+import JobList from "./JobList";
 
 const BASE_URL = "https://api.ziprecruiter.com/jobs/v1";
-console.log('env: ', process.env)
+console.log("env: ", process.env);
 const { REACT_APP_ZIP_RECRUITER_API_KEY } = process.env;
 
-async function zipFetch (location: string, search: string) {
-  const fullUrl = encodeURI(BASE_URL + `?api_key=${REACT_APP_ZIP_RECRUITER_API_KEY}&location=${location}&search=${search}`);
+async function zipFetch(location: string, search: string) {
+  const fullUrl = encodeURI(
+    BASE_URL +
+      `?api_key=${REACT_APP_ZIP_RECRUITER_API_KEY}&location=${location}&search=${search}`
+  );
   console.log(fullUrl);
   return (await fetch(fullUrl, {})).json();
-};
+}
 
+type searchStatus = 'noSearches' | 'notFound' | 'success'
 interface AppState {
   location: string;
   search: string;
   jobList: Array<any>;
+  searchStatus: searchStatus;
 }
 
 const App: React.FC = () => {
-  const [appState, setState] = useState({location: 'portland', search: 'python', jobList: []} as AppState);
-  const {location, search, jobList} = appState;
-  // useEffect(() => {
-  //   zipFetch(location, search)
-  //     .then((out: any) => {
-  //       console.log(out);
-  //       setState({...appState, jobList: out.jobs})
-  //     });
-  // });
+  const [appState, setState] = useState({
+    location: "",
+    search: "",
+    jobList: [],
+    searchStatus: 'noSearches'
+  } as AppState);
+  const { location, search, jobList, searchStatus } = appState;
 
-  function onSubmit (e: React.FormEvent<HTMLFormElement>) {
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log( 'submitting');
+    console.log("submitting");
 
-    zipFetch(location, search)
-      .then((data: any) => {
-        console.log('data: ', data);
-        setState({...appState, jobList: data.jobs})
-      });
+    zipFetch(location, search).then((data: any) => {
+      console.log("data: ", data);
+      setState({ ...appState, jobList: data.jobs, searchStatus: data.jobs.length > 0 ? 'success' : 'notFound' });
+    });
+  }
+
+  function onSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setState({ ...appState, search: e.target.value });
+  }
+
+  function onlocationChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setState({ ...appState, location: e.target.value });
+  }
+  const statusMessageMap = {
+    noSearches: () => <h3 className="usage-prompt no-searches">Time to look for a job!</h3>,
+    notFound: () => <h3 className="usage-prompt not-found">No jobs matching these search terms found!</h3>,
+    success: () => <h3 className="usage-prompt success">Jobs found!</h3>
   };
 
-  function onSearchChange (e: React.ChangeEvent<HTMLInputElement>) {
-    setState({...appState, search: e.target.value});
-  }
-
-  function onlocationChange (e: React.ChangeEvent<HTMLInputElement>) {
-    setState({...appState, location: e.target.value});
-  }
+  const statusMessage = statusMessageMap[searchStatus]();
 
   return (
     <div className="App">
@@ -55,21 +64,33 @@ const App: React.FC = () => {
       </nav>
       <div className="content-container">
         <div className="search-container">
-          <h3 className="usage-prompt">Time to look for a job!</h3>
+          {statusMessage}
           <form id="search-form" onSubmit={onSubmit}>
             <label className="location-label">Location</label>
-            <input name="location" type="text" value={location} onChange={onlocationChange} placeholder="location"/>
+            <input
+              name="location"
+              type="text"
+              value={location}
+              onChange={onlocationChange}
+              placeholder="location"
+            />
             <label className="keywords-label">Keywords</label>
-            <input name="keywords" type="text" value={search} onChange={onSearchChange} placeholder="job keywords"/>
+            <input
+              name="keywords"
+              type="text"
+              value={search}
+              onChange={onSearchChange}
+              placeholder="job keywords"
+            />
             <div className="button-row">
-              <input type="submit"/>
+              <input type="submit" />
             </div>
           </form>
-        <JobList jobs={jobList} />
+          <JobList jobs={jobList} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
