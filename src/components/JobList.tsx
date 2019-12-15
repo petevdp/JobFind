ef09d9c828a3a9afae7ac3f8fa3e83c02c9409f2
grey275 <pjvanderpol@gmail.com>
2019-12-15 13:45:40 -0800

    delete dead code

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,38 +1,6 @@
 import React, { useState, useReducer, useEffect } from 'react';
 import { job} from './types';
 import JobPosting from './JobPosting';
-
-
-/** properties to compare from a job, in precedence order
- *  for compareJobElements */
-const keysToCompare = [
-  'city',
-  'state',
-  'country'
-]
-
-/**
- * determines which element should go first in a list
- * a and b passed as any to allow keysToCompare values
- * to not raise typeerrors
- *
- *
- * unused right now, doing only server side sorting
-*/
-const compareJobElements = (a: any, b: any) => {
-  for (let keyToCompare of keysToCompare) {
-      let aHasKey = a[keyToCompare] !== "";
-      if (aHasKey === bHasKey) {
-        continue;
-      } else if (aHasKey && !bHasKey) {
-        return -1;
-      } else {
-        return 1
-      }
-  }
-  return 0;
-}
-
 interface JobListProps {
   jobs: Array<job>;
 };
