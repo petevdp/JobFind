db648f004a4db1830569d0db6c5435b124662e3f
grey275 <pjvanderpol@gmail.com>
2019-12-21 11:51:55 -0800

    complete type 'job' based on api output

--- src/types.ts
+++ src/types.ts
@@ -11,10 +11,24 @@ export interface job {
   city: string;
   hiring_company: hiringCompany;
   posted_time: string;
+  country: string;
+  buyer_type: string;
+  industry_name: string;
+  has_zipapply: boolean;
+  last_plan_name: string | null;
+  category: string;
   posted_time_friendly: string;
-  location: "us";
+  location: string;
   snippet: string;
   url: string;
   salary_max_annual: number;
   salary_min_annual: number;
+  salary_interval: string;
+  has_non_zr_url: string;
+  job_age: number;
+  salary_source: string;
+  state: string;
+  salary_max: number;
+  salary_min: number;
+  source: string;
 }
